ee11922c58b9504481c77c36e9d1edd70e84d627
unknown <kathie@gatech.edu>
2024-12-04 12:39:23 -0500

    fix indent

--- src/server/app.py
+++ src/server/app.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-            # TODO send signals out rpi gpio to start coffee making
-            if "start" in request.form:
-                return 'Starting coffee making!'
-            elif "cancel" in request.form:
-                return 'Cancelling brewing'
+		print(request.form)
+        # TODO send signals out rpi gpio to start coffee making
+        if "start" in request.form:
+            return 'Starting coffee making!'
+        elif "cancel" in request.form:
+            return 'Cancelling brewing'
 	return render_template('coffee.html')
 # @app.route('/newpage')
 # def newpage():
 	# return 'Second page if needed'
 	
-# if __name__ == '__main__':
-# app.run(debug = True, host = '0.0.0.0')
+if __name__ == '__main__':
+app.run(debug = True, host = '0.0.0.0')
 
