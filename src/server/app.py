22f895ace27213be156597756d7022fe919af6a1
kathiehyu <kathiehyu@gmail.com>
2024-12-05 00:00:49 +0000

    retrieve preset time

--- src/server/app.py
+++ src/server/app.py
@@ -8,15 +8,17 @@ def index():
 	if request.method == 'POST':
 		print(request.form)
         	# TODO send signals out rpi gpio to start coffee making
-        	if "start" in request.form:
-           		return 'Starting coffee making!'
-        	elif "cancel" in request.form:
-            		return 'Cancelling brewing'
-		return render_template('coffee.html')
+		if "start" in request.form:
+			return 'Starting coffee making!'
+		elif "cancel" in request.form:
+			return 'Cancelling brewing'
+		elif "submit-brew-time" in request.form:
+			return 'Brewing time set to ' + request.form['time']
+	return render_template('coffee.html')
 # @app.route('/newpage')
 # def newpage():
 	# return 'Second page if needed'
-	
+
 if __name__ == '__main__':
-app.run(debug = True, host = '0.0.0.0')
+	app.run(debug = True, host = '0.0.0.0')
 
